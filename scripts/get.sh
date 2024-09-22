4ff8f9203f28c2743980c8abff8715d93448fe67
Anim Mouse <git@animmouse.com>
2024-09-23 02:01:41 +0800

    fix: Fix variable not expanding, and silent cURL

--- scripts/get.sh
+++ scripts/get.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
-curl -X GET "https://api.cloudflare.com/client/v4/accounts/$account_id/pages/projects/$project_name" \
+curl -sS -X GET "https://api.cloudflare.com/client/v4/accounts/$account_id/pages/projects/$project_name" \
      -H "Authorization: Bearer $api_token" \
      -H "Content-Type:application/json" \
-     | jq -r .result.deployment_configs.production.env_vars.$variable_name.value \
-     | echo value=$(</dev/stdin) >> $GITHUB_OUTPUT
+     | jq -r ".result.deployment_configs.production.env_vars.$variable_name.value" \
+     | echo "value=$(</dev/stdin)" >> $GITHUB_OUTPUT
--- scripts/update.sh
+++ scripts/update.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 set -eu
-curl -X PATCH "https://api.cloudflare.com/client/v4/accounts/$account_id/pages/projects/$project_name" \
+curl -sSo /dev/null -X PATCH "https://api.cloudflare.com/client/v4/accounts/$account_id/pages/projects/$project_name" \
      -H "Authorization: Bearer $api_token" \
      -H "Content-Type:application/json" \
      -d '{
           "deployment_configs": {
             "production": {
               "env_vars": {
-                "$variable_name": {
-                  "value": "$variable_value"
+                "'"$variable_name"'": {
+                  "value": "'"$variable_value"'"
                 }
               }
             },
             "preview": {
               "env_vars": {
-                "$variable_name": {
-                  "value": "$variable_value"
+                "'"$variable_name"'": {
+                  "value": "'"$variable_value"'"
                 }
               }
             }
