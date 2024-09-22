4c544cfe09bb5f8e835dccb3b28c9c489831a63d
Anim Mouse <git@animmouse.com>
2024-09-23 01:30:57 +0800

    Initial commit

---
+++ update.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -eu
+curl -X PATCH "https://api.cloudflare.com/client/v4/accounts/$account_id/pages/projects/$project_name" \
+     -H "Authorization: Bearer $api_token" \
+     -H "Content-Type:application/json" \
+     -d '{
+          "deployment_configs": {
+            "production": {
+              "env_vars": {
+                "$variable_name": {
+                  "value": "$variable_value"
+                }
+              }
+            },
+            "preview": {
+              "env_vars": {
+                "$variable_name": {
+                  "value": "$variable_value"
+                }
+              }
+            }
+          }
+        }'
