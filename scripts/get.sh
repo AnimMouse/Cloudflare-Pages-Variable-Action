2b5cf3b36d6c4597f12993d22eaef38deb62d52b
Anim Mouse <git@animmouse.com>
2024-09-23 14:32:15 +0800

    refactor: Add pipefail and use variable_value inside composite

--- scripts/get.sh
+++ scripts/get.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-set -eu
+set -euo pipefail
 curl -sS -X GET "https://api.cloudflare.com/client/v4/accounts/$account_id/pages/projects/$project_name" \
      -H "Authorization: Bearer $api_token" \
      -H "Content-Type:application/json" \
      | jq -r ".result.deployment_configs.production.env_vars.$variable_name.value" \
-     | echo "value=$(</dev/stdin)" >> $GITHUB_OUTPUT
+     | echo "variable_value=$(</dev/stdin)" >> $GITHUB_OUTPUT
