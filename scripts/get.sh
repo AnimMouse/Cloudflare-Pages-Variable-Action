30b32ad719f44b7960668f54c3b5d438fdc2f39c
Anim Mouse <git@animmouse.com>
2024-09-23 14:19:33 +0800

    fix: Try to use bash

--- scripts/get.sh
+++ scripts/get.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -eu
 curl -sS -X GET "https://api.cloudflare.com/client/v4/accounts/$account_id/pages/projects/$project_name" \
      -H "Authorization: Bearer $api_token" \
